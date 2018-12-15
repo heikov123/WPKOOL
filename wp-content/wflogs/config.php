03422344643b1387645572079b7a3be6046d907e
heikovalliiktkhk <heiko.valli@khk.ee>
2018-12-15 18:32:17 +0200

    modified file `wflogs` (after activation of `Wordfence Security` version 7.1.18)

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1545496331;s:7:"authKey";s:64:"R|> X5-P1x(ph;3c*_[Xy:*^~QbzbN)U!o.CCy@hfnceU631L$h9SG]_k^q$Lt@t";}
